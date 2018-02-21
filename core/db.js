497c1c92f55546743aca170ab63a8fabc4ac0488
Rohit <rohit.gorana@live.in>
2018-02-22 00:57:22 +0530

    mongodb connection string update

--- core/db.js
+++ core/db.js
@@ -1,7 +1,7 @@
 var mongoose  = require('mongoose')
 
 module.exports = (function(){
-    mongoose.connect('mongodb://localhost/spotme')
+    mongoose.connect('mongodb+srv://rohitgorana:brandnewday@cluster0-cncd3.mongodb.net/spotme')
       
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
